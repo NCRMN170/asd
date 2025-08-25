cfb394323d653a96caf9a60eece633a45a62e6d0
NCRMN170 <kmet4686@gmail.com>
2025-08-26 00:35:39 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_TB132FU.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_TB132FU-eng \
-    twrp_TB132FU-userdebug \
-    twrp_TB132FU-user
+    TB132FU-eng \
+    TB132FU-userdebug \
+    TB132FU-user
